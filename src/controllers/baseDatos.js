d8acada932f19c1714a5db76ac6f3d21873f47ee
diseño y desarrollo <disenoydesarrollo@academicos.cesde.edu.co>
2024-09-20 07:23:54 -0500

    Se crea arreglo de objetos

---
+++ src/controllers/baseDatos.js
@@ -0,0 +1,15 @@
+let gastos = Array(
+    {
+        id: 1,
+        descripcion: "Pasaje en bus",
+        monto: 8000,
+        fecha: "2024-9-20"
+    },
+    {
+        id: 2,
+        descripcion: "Empanada de pollo + cocacola zero",
+        monto: 10000,
+        fecha: "2024-9-20"
+    }
+);
+
