edcda49ed71442b05db6b768ab094bf5ef2b6ff1
Albeiro Muriel <jamurielve@cesde.net>
2024-09-20 07:23:25 -0500

    se crea archivo para emular la BD

---
+++ src/controllers/BaseDatos.js
@@ -0,0 +1,18 @@
+let gastosDos=Array(
+    {
+        id:1,
+        descripcion:"Pasaje de sabaneta a cesde",
+        monto:8000,
+        fecha:"2024-9-20"
+    },
+    {
+        id:2,
+        descripcion:"Empanada de pollo + cocacola zero",
+        monto:10000,
+        fecha:"2024-9-20"
+    }
+
+)
+
+
+
