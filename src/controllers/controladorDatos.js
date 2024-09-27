2569e79d99e198575eb58178174761d9e8f02e95
Albeiro Muriel <jamurielve@cesde.net>
2024-09-27 06:20:17 -0500

    ok

---
+++ src/controllers/controladorDatos.js
@@ -0,0 +1,5 @@
+import { gastos } from "./simuladorBD.js";
+
+console.log(gastos)
+
+//Yo como cliente quisiera poder ver los gastos mayores a 10000
---
+++ src/controllers/simuladorBD.js
@@ -0,0 +1,72 @@
+export let gastos=Array(
+    {
+        id:5,
+        descripcion:"Pasajes del lunes",
+        monto:8000,
+        fecha:"2024-9-23",
+        categoria:"transporte"
+    },
+    {
+        id:15,
+        descripcion:"Empanada de atun",
+        monto:3000,
+        fecha:"2024-9-23",
+        categoria:"comida"
+    },
+    {
+        id:21,
+        descripcion:"cereal con kumis",
+        monto:5000,
+        fecha:"2024-9-24",
+        categoria:"comida"
+    },
+    {
+        id:80,
+        descripcion:"Pago spotify",
+        monto:2100,
+        fecha:"2024-9-23",
+        categoria:"entretenimiento"
+    },
+    {
+        id:20,
+        descripcion:"transporte casa mamá",
+        monto:10500,
+        fecha:"2024-9-23",
+        categoria:"transporte"
+    },
+    {
+        id:200,
+        descripcion:"corrientazo de pescado",
+        monto:16000,
+        fecha:"2024-9-24",
+        categoria:"comida"
+    },
+    {
+        id:150,
+        descripcion:"Pasajes comfama estrella",
+        monto:8000,
+        fecha:"2024-9-29",
+        categoria:"transporte"
+    },
+    {
+        id:785,
+        descripcion:"Empanada de pollo",
+        monto:3000,
+        fecha:"2024-9-26",
+        categoria:"comida"
+    },
+    {
+        id:158,
+        descripcion:"Nacional vs Cali",
+        monto:40000,
+        fecha:"2024-9-29",
+        categoria:"entretenimiento"
+    },
+    {
+        id:241,
+        descripcion:"media de guaro+cover 2x1",
+        monto:18000,
+        fecha:"2024-9-21",
+        categoria:"Entretenimiento"
+    }
+)
